e548fe387a7f1f9c0742da7a1406617eab7dd1be
Seas00n <60185289+Seas00n@users.noreply.github.com>
2023-07-12 21:59:12 +0800

    11
    1

--- canopen_decoder.py
+++ canopen_decoder.py
@@ -5,8 +5,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-# file_path = "c2.xltm"
-# sheet_name = "c2"
+# file_path = "c5.xltx"
+# sheet_name = "c5"
 # df = pd.read_excel(file_path, sheet_name=sheet_name)
 #
 # num_rows = len(df["序号"])
@@ -31,10 +31,10 @@ import matplotlib.pyplot as plt
 # len_array = np.array(len_list)
 # data_array = np.array(data_list)
 # time_array = np.array(time_list)
-# np.save("c2_id.npy", id_array)
-# np.save("c2_len.npy", len_array)
-# np.save("c2_data.npy", data_array)
-# np.save("c2_time.npy", time_array)
+# np.save("c5_id.npy", id_array)
+# np.save("c5_len.npy", len_array)
+# np.save("c5_data.npy", data_array)
+# np.save("c5_time.npy", time_array)
 
 
 def twosComplement_hex(hexval, bits):
@@ -48,10 +48,10 @@ def twosComplement_hex(hexval, bits):
     return signed_int
 
 
-time_array = np.load("c2_time.npy", allow_pickle=True)
-id_array = np.load("c2_id.npy", allow_pickle=True)
-data_array = np.load("c2_data.npy", allow_pickle=True)
-len_array = np.load("c2_len.npy", allow_pickle=True)
+time_array = np.load("c5_time.npy", allow_pickle=True)
+id_array = np.load("c5_id.npy", allow_pickle=True)
+data_array = np.load("c5_data.npy", allow_pickle=True)
+len_array = np.load("c5_len.npy", allow_pickle=True)
 num_msg = np.size(time_array, 0)
 id = "0x7e"
 pos_list = []
@@ -63,9 +63,12 @@ vel_list = []
 torque_list = []
 vel_torque_time_list = []
 vel_torque_idx_list = []
+current_cmd_list = []
+current_cmd_idx_list = []
 
 tpdo_motor1 = "0x01FE"
 tpdo_motor2 = "0x02FE"
+cmd_sdo = "0x067E"
 for i in range(num_msg):
     if id_array[i] == tpdo_motor1:
         str_data = data_array[i]
@@ -107,11 +110,33 @@ for i in range(num_msg):
         time_ = time_array[i]
         time_ = time_.hour * 3600 + time_.minute * 60 + time_.second + time_.microsecond * 0.001
         vel_torque_time_list.append(time_)
+    elif id_array[i] == cmd_sdo:
+        str_data = data_array[i]
+        len = len_array[i]
+        if len < 8:
+            continue
+        data_ = str_data.split(" ", len)
+        if data_[1] == '71' and data_[2] == '60':  # catch current cmd
+            current_cmd_ = data_[5] + data_[4]
+            current_cmd_ = twosComplement_hex(current_cmd_, 16)
+            current_cmd_list.append(current_cmd_)
+            current_cmd_idx_list.append(i)
 
-d_array = np.array(state_word_list)*0.01
-d2_array = np.array(pos_list)
-idx = np.arange(np.size(d_array, 0))
-idx_2 = np.arange(np.size(d2_array, 0))
-plt.plot(idx, d_array)
-plt.plot(idx_2, d2_array)
+d_array = np.array(state_word_list) * 0.08
+d2_array = np.array(current_list)*0.05
+d3_array = np.array(current_cmd_list) * 0.05
+idx = np.array(pos_current_idx_list)
+idx_2 = np.array(pos_current_idx_list)
+idx_3 = np.array(current_cmd_idx_list)
+plt.plot(idx, d_array, label='stateword')
+plt.plot(idx_2, d2_array, label='position')
+plt.plot(idx_3, d3_array, label='current_cmd')
+plt.legend()
 plt.show()
+last_state = state_word_list[0]
+print('{:016b}'.format(last_state))
+for i in range(np.size(d_array,0)):
+    if state_word_list[i] != last_state:
+        last_state = state_word_list[i]
+        print('{:016b}'.format(last_state))
+        print(pos_current_idx_list[i])
